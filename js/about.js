5ac08f42d70ffb6a063b7566612140eeb9a12062
Arshi <arshij>
2021-05-31 00:14:07 -0700

    new design

--- js/about.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-  $('#aLittleMore').on('shown.bs.collapse', function() {
-    $("#about-more").text("- a little bit more about me");;
-  })
-  $('#aLittleMore').on('hidden.bs.collapse', function() {
-    $("#about-more").text("+ a little bit more about me");;
-  })
-})(jQuery);
